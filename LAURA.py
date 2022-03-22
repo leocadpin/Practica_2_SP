a05cc991ec756f3b03f0c86f88b9b6f20d940d2a
leocadpin <leopoldocadavid@gmail.com>
2022-03-22 10:41:03 +0100

    vikimarika

--- LAURA.py
+++ LAURA.py
@@ -17,17 +17,25 @@ pcd.points = o3d.utility.Vector3dVector(np.array(points))
 # Leer nube de puntos
 pcd = o3d.io.read_point_cloud("snap_0point.pcd")
 
-#distance_threshold --> rango max/menos plano dominante por eso al poner 1 pilla la mesa (en m) 
-#ransac_n ---> al tratarse de un plano son 3 puntos
-#num_iteration ---> num veces ejecuta ransac
+# distance_threshold --> rango max/menos plano dominante por eso al poner 1 pilla la mesa (en m) 
+# ransac_n ---> al tratarse de un plano son 3 puntos
+# num_iteration ---> num veces ejecuta ransac
 
-plane_model, inliers = pcd.segment_plane(distance_threshold = 0.1,ransac_n=3,num_iterations=1000)
-#[a,b,c,d] = plane_model
+# El threshol es la grosor del plano dominante
+plane_model, inliers = pcd.segment_plane(distance_threshold = 0.025,ransac_n=3,num_iterations=1000)
+# [a,b,c,d] = plane_model
 inlier_cloud = pcd.select_by_index(inliers)
 outlier_cloud = pcd.select_by_index(inliers,invert=True)
 inlier_cloud.paint_uniform_color([1.0,0,0])
 
+pcd2 = outlier_cloud
 
+# El threshol es la grosor del plano dominante
+plane_model, inliers = pcd2.segment_plane(distance_threshold = 0.025,ransac_n=3,num_iterations=1000)
+# [a,b,c,d] = plane_model
+inlier_cloud = pcd2.select_by_index(inliers)
+outlier_cloud = pcd2.select_by_index(inliers,invert=True)
+inlier_cloud.paint_uniform_color([0,1.0,0])
 
 
 # Mostrar nube de puntos
