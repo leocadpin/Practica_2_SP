a5cfdbac39fdb6c5cd77ddbf268091be1fdd15c7
Victoria293 <victoria.nik007@gmail.com>
2022-04-11 11:53:03 +0200

    Comentarios

--- LAURA.py
+++ LAURA.py
@@ -49,37 +49,40 @@ inlier_cloud.paint_uniform_color([0,1.0,0])
 pcd3 = outlier_cloud
 
 # Mostrar nube de puntos
-#o3d.visualization.draw_geometries([outlier_cloud]) 
-# 
-# # #tambien se ppued usar UNIFORM SAMPLING PARA RESUMIR PUNTOS
+# o3d.visualization.draw_geometries([outlier_cloud]) 
+# tambien se ppued usar UNIFORM SAMPLING PARA RESUMIR PUNTOS
 
 print('input')
-#N = 2000
-#pcd = o3dtut.get_armadillo_mesh().sample_points_poisson_disk(N)
+# N = 2000
+# pcd = o3dtut.get_armadillo_mesh().sample_points_poisson_disk(N)
 # fit to unit cube
-#pcd3.scale(1 / np.max(pcd3.get_max_bound() - pcd3.get_min_bound()), #no es necesario para estae problema 
-          #center=pcd.get_center())
-#pcd3.colors = o3d.utility.Vector3dVector(np.random.uniform(0, 1, size=(N, 3)))
-#o3d.visualization.draw_geometries([pcd3])
+# pcd3.scale(1 / np.max(pcd3.get_max_bound() - pcd3.get_min_bound()), # no es necesario para estae problema 
+# center=pcd.get_center())
+# pcd3.colors = o3d.utility.Vector3dVector(np.random.uniform(0, 1, size=(N, 3)))
+# o3d.visualization.draw_geometries([pcd3])
 
 print('voxelization')
 
-#voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd3,
-#                                                            voxel_size=0.01)
-#o3d.visualization.draw_geometries([voxel_grid])
+# voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd3, voxel_size=0.01)
+# o3d.visualization.draw_geometries([voxel_grid])
+
+# Crea un voxel grid e formato de nube de puntos
 voxel_grid = pcd3.voxel_down_sample(0.01)
 print(voxel_grid)
-o3d.visualization.draw_geometries([voxel_grid])
-tic = time.time()
 
+o3d.visualization.draw_geometries([voxel_grid])
+
+# Detectamos los keypoints
+tic = time.time()
 keypoints = o3d.geometry.keypoint.compute_iss_keypoints(voxel_grid)
 toc = 1000 * (time.time() - tic)
 
 print("ISS Computation took {:.0f} [ms]".format(toc))
 print(keypoints) 
-#o3d.visualization.draw_geometries([keypoints])
+# o3d.visualization.draw_geometries([keypoints])
 
-
-# #pcd3.paint_uniform_color([0.5, 0.5, 0.5])
+# pcd3.paint_uniform_color([0.5, 0.5, 0.5])
 # keypoints.paint_uniform_color([255.0, 0, 0.0])
 o3d.visualization.draw_geometries([keypoints])
+
+# Descriptores
